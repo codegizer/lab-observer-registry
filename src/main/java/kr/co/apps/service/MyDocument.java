592e3eefa034106ba4066c6f942239c2bd8ffd3b
내codegizer <codegizer.kim@gmail.com>
2020-04-21 15:19:46 +0000

    코드정렬이 깨진것 보완

--- src/main/java/kr/co/apps/service/MyDocument.java
+++ src/main/java/kr/co/apps/service/MyDocument.java
@@ -13,50 +13,50 @@ import java.util.*;
 public class MyDocument {
 	
 	//옵저버관리서비스
-    @Autowired
+	@Autowired
 	ServiceRegistry serviceRegistry;
     
-    int key = 0;
+	int key = 0;
     
 	//Subject 엔티티 관리를 위한 해시맵
-    Map<Integer,MyDocumentVo> dataHashMap = new HashMap<Integer,MyDocumentVo>();
+	Map<Integer,MyDocumentVo> dataHashMap = new HashMap<Integer,MyDocumentVo>();
     
 	//Subject 엔티티 목록 리턴
-    public List<MyDocumentVo> selectDocList()
-    {
+	public List<MyDocumentVo> selectDocList()
+	{
 		List<MyDocumentVo> list = new ArrayList<MyDocumentVo>();
 		
 		Iterator<Integer> keys = dataHashMap.keySet().iterator();
 		
 		while(keys.hasNext())
 		{
-            list.add(dataHashMap.get(keys.next()));
+			list.add(dataHashMap.get(keys.next()));
 		}
 		
 		return list;
-    }
+	}
     
 	//Subject 엔티티 추가
-    public synchronized void add(String title, String taskId)
-    {
-        MyDocumentVo vo = new MyDocumentVo();
+	public synchronized void add(String title, String taskId)
+	{
+		MyDocumentVo vo = new MyDocumentVo();
         
-        key++;
+		key++;
         
-        vo.setKey(key);
-        vo.setTitle(title);
-        vo.setTaskId(taskId);
+		vo.setKey(key);
+		vo.setTitle(title);
+		vo.setTaskId(taskId);
         
-        dataHashMap.put(key,vo);
+		dataHashMap.put(key,vo);
     }
     
 	//notify
 	//Subject와 관련된 연계서비스를 호출
     public void callEvtExecute(int docKey, CbEventType eventType)
     {
-        MyDocumentVo docVo = dataHashMap.get(docKey);
-        String taskId = docVo.getTaskId();
-        serviceRegistry.notify(docVo,taskId,eventType);
+		MyDocumentVo docVo = dataHashMap.get(docKey);
+		String taskId = docVo.getTaskId();
+		serviceRegistry.notify(docVo,taskId,eventType);
         
     }
 }
